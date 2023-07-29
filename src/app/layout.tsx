0f09125386eec888815ffe30db579065a702bb0c
0917ba <0917ba@naver.com>
2023-07-29 18:47:34 +0900

    feat: decorate navbar apply tailwindcss and decorate navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,23 @@
-import Link from 'next/link';
+import NavBar from '@/components/NavBar';
+import './globals.css';
+import { Noto_Sans_KR } from 'next/font/google';
+
+const noto_sans_kr = Noto_Sans_KR({
+  weight: ['100', '300', '400', '500', '700', '900'],
+  subsets: ['latin'],
+  display: 'swap',
+});
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang='ko'>
-      <body>
+    <html lang="ko" className={noto_sans_kr.className}>
+      <body className="light bg-slate-50 text-slate-900 antialiased">
         <header>
-          <h2>익명게시판😀</h2>
           <nav>
-            <Link href='/write'>
-              <button>글쓰기</button>
-            </Link>
+            <NavBar />
           </nav>
         </header>
-        <main>{children}</main>
+        <main className="pt-14">{children}</main>
       </body>
     </html>
   );
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+
+export default function NavBar() {
+  return (
+    <div className="fixed inset-x-0 top-0 z-[10] h-fit border-b border-zinc-300 bg-zinc-100 py-2">
+      <div className="flex h-full items-center justify-around gap-5 px-2">
+        <div className="text-lg font-normal text-zinc-700">익명게시판😀</div>
+        <div className="flex h-8 max-w-xl grow items-center justify-start rounded-md bg-white px-2 ">
+          <div className=" font-light text-zinc-700">검색</div>
+        </div>
+        <Link
+          href="/write"
+          className=" flex h-8 w-16 items-center justify-center rounded-md bg-zinc-900"
+        >
+          <div className="text-sm font-light text-white">글쓰기</div>
+        </Link>
+      </div>
+    </div>
+  );
+}
