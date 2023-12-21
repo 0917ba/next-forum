2361fa14376b5c9acffc0fb38f1cf3b8741cdbce
0917ba <0917ba@naver.com>
2023-12-21 09:27:22 +0900

    feat: Add metadata(title)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,18 @@ import NavBar from "@/components/page/NavBar";
 import "./globals.css";
 import { Noto_Sans_KR } from "next/font/google";
 import AuthSession from "@/components/auth/AuthSession";
+import type {Metadata} from 'next'
 
 const noto_sans_kr = Noto_Sans_KR({
   weight: ["100", "300", "400", "500", "700", "900"],
   subsets: ["latin"],
   display: "swap",
 });
+
+export const metadata: Metadata = {
+    title: "인곽게시판😃",
+    description: "인천과학고 커뮤니티 게시판",
+}
 
 export default function RootLayout({
   children,
