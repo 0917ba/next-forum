46ae863606aca52e5beacd4c5482082deac1f664
0917ba <0917ba@naver.com>
2023-12-25 01:43:47 +0900

    fix: Fix an issue about deleting posts

--- src/components/post/CommentDeleteBtn.tsx
+++ src/components/post/CommentDeleteBtn.tsx
@@ -1,19 +1,16 @@
 "use client";
 
-import { useRouter } from "next/navigation";
 import CoolButton from "../ui/CoolButton";
 
 export default function CommentDeleteBtn({ id }: { id: string }) {
-  const router = useRouter();
-
   const onDelete = async () => {
     await fetch(`/api/comments/${id}`, {
       method: "DELETE",
     });
     alert("삭제되었습니다.");
-    router.refresh();
+    // router.refresh();
     // refresh
-    // location.reload();
+    location.reload();
   };
   return <CoolButton onClick={onDelete}>삭제</CoolButton>;
 }
